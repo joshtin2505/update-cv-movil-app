e39d4cbf081a7758259cd2369589f3729a0a15f2
joshtin2505 <justincastro2505@gmail.com>
2024-04-25 16:27:43 -0500

    Add navigation and screens for HomeScreen and CVFormScreen

---
+++ screens/CVFormScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native"
+import React from "react"
+
+const CVFormScreen = () => {
+  return (
+    <View>
+      <Text>CVFormScreen</Text>
+    </View>
+  )
+}
+
+export default CVFormScreen
